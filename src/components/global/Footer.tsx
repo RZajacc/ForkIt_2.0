8f91a11492cbbe8d494963e31a990401f6abcfbf
RZajacc <rf.zajac@gmail.com>
2024-04-16 15:38:12 +0200

    added a footer with styling

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,14 +1,31 @@
-import { Container, Row } from 'react-bootstrap'
-
+import "../../style/footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <Container fluid id='footer'>
-      <Row className='text-center'>
-        <p>&reg;Rafal Zajac</p>
-      </Row>
-    </Container>
-  )
+    <footer className="footer">
+      <ul className="footer__items">
+        <li className="footer__item">
+          <Link to={"https://github.com/RZajacc"}>
+            <FontAwesomeIcon icon={faGithub} className="fa-xl" />
+          </Link>
+        </li>
+        <li className="footer__item">
+          <Link to={"https://www.linkedin.com/in/rafalzajac88/"}>
+            <FontAwesomeIcon icon={faLinkedin} className="fa-xl" />
+          </Link>
+        </li>
+        <li className="footer__item">
+          <Link to={"contact"}>
+            <FontAwesomeIcon icon={faEnvelope} className="fa-xl" />
+          </Link>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
