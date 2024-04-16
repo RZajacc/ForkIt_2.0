47eff3dbbc76e1cec8c156aec3b9c633cd321ecc
RZajacc <rf.zajac@gmail.com>
2024-04-16 13:30:40 +0200

    added conditional display of logout and dasbhoard depending on if user is logged in

--- src/components/global/AppNav.tsx
+++ src/components/global/AppNav.tsx
@@ -34,81 +34,28 @@ function AppNav() {
             <span></span>
           </button>
         </nav>
-
-        {/* <div className={mobileNavState}> */}
-        {/* <Link to={"recipes"}>Recipes</Link> */}
-        {/* <Link to={"contact"}>Contact</Link> */}
-        {/* Here will be some logic for user */}
-        {/* <Link to={"account"}>Account</Link> */}
-        {/* </div> */}
-        {/* <ul className="mobile-nav__items">
-          <li className="mobile-nav_item">
-            <Link to={"recipes"}>Recipes</Link>
-          </li>
-          <li className="mobile-nav_item">
-            <Link to={"contact"}>Recipes</Link>
-          </li>
-          {/* Here will be some logic for user */}
-        {/* <li className="mobile-nav_item">
-            <Link to={"account"}>Account</Link>
-          </li>
-        </ul> */}
       </header>
       <div className={mobileNavState}>
         <Link to={"recipes"}>Recipes</Link>
         <Link to={"contact"}>Contact</Link>
         {/* Here will be some logic for user */}
-        <Link to={"account"}>Account</Link>
+
+        {user ? (
+          <>
+            <Link to={"dashboard"}>Dashboard</Link>
+            <Link to={"account"} onClick={logout}>
+              Logout
+            </Link>
+          </>
+        ) : (
+          <Link to={"account"}>Account</Link>
+        )}
       </div>
     </>
 
-    // <Navbar expand='sm' className='p-3'>
-    //   <Container fluid>
-    //     <LinkContainer to={'/'}>
-    //       <Navbar.Brand>
-    //         <img
-    //           src='https://firebasestorage.googleapis.com/v0/b/forkit-d574f.appspot.com/o/hungry.svg?alt=media&token=a0bc267d-5ae9-42ba-b93e-9fc4d216eb51'
-    //           width='40'
-    //           height='40'
-    //           className='d-inline-block align-top'
-    //           alt='Forkit logo'
-    //         />
-    //         Forkit
-    //       </Navbar.Brand>
-    //     </LinkContainer>
-    //     <Navbar.Toggle aria-controls='basic-navbar-nav' />
-    //     <Navbar.Collapse id='basic-navbar-nav' className='justify-content-end'>
-    //       <Nav>
-    //         <LinkContainer to={'recipes'}>
-    //           <NavLink>Recipes</NavLink>
-    //         </LinkContainer>
-    //         <LinkContainer to={'contact'}>
-    //           <NavLink>Contact</NavLink>
-    //         </LinkContainer>
-
-    //         {user ? (
-    //           <>
-    //             <LinkContainer to={'dashboard'}>
-    //               <NavLink>Dashboard</NavLink>
-    //             </LinkContainer>
-    //             <LinkContainer to={'account'}>
-    //               <Button variant='danger' onClick={logout}>
     //                 <img src='https://firebasestorage.googleapis.com/v0/b/forkit-d574f.appspot.com/o/door-open-white.svg?alt=media&token=02cc62e2-e221-46ba-acfe-9ce5e156a510' alt='opened door' className='nav-button-image' />{' '}
     //                 Logout
-    //               </Button>
-    //             </LinkContainer>
-    //           </>
-    //         ) : (
-    //           <LinkContainer to={'account'}>
-    //             <NavLink>
     //               <img src='https://firebasestorage.googleapis.com/v0/b/forkit-d574f.appspot.com/o/account-50-black.png?alt=media&token=07ba0644-e81a-4f25-8166-a4d0d6c7a799' className='nav-button-image' /> Account
-    //             </NavLink>
-    //           </LinkContainer>
-    //         )}
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
   );
 }
 
