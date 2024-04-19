69943617d1ae165a70bc31e9f7e69af162f72712
RZajacc <rf.zajac@gmail.com>
2024-04-19 16:45:51 +0200

    temporarily finished styling recipe cards and list of cards as a whole

--- src/components/recipesView/recipeCard/RecipeCard.tsx
+++ src/components/recipesView/recipeCard/RecipeCard.tsx
@@ -10,15 +10,17 @@ type Props = {
 function RecipeCard({ recipe, id }: Props) {
   return (
     <div className="recipe-card" key={id}>
-      <section className="recipe-card__image-section">
-        <img src={recipe.image} alt="recipe-image" />
+      <section className="recipe-card__header-section">
         <p className="recipe-card__cooking-time">
-          Time: <span>{recipe.readyInMinutes}</span> min.
+          Time:<span> {recipe.readyInMinutes}</span> min.
         </p>
         <p className="recipe-card__nutritional-score">
           Score:
-          <span> {recipe.healthScore}</span> pts.
+          <span>{recipe.healthScore}</span>
         </p>
+      </section>
+      <section className="recipe-card__image-section">
+        <img src={recipe.image} alt="recipe-image" />
       </section>
       <p className="recipe-card__recipe-title">{recipe.title}</p>
       <section className="recipe-card__read-more-link">
