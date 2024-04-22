b7deca77a5ef8a5bc3b85ba912390cf7f0993e93
RZajacc <rf.zajac@gmail.com>
2024-04-22 15:44:17 +0200

    finished styling recipe detaiils

--- src/views/RecipeDetails.tsx
+++ src/views/RecipeDetails.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
-import { RecipeGeneral, userFavs } from "../types/types";
-import Comments from "../components/Comments";
+import { useContext, useEffect, useState } from "react";
+import { db } from "../config/firebaseConfig";
 import {
   collection,
   addDoc,
@@ -10,11 +10,10 @@ import {
   deleteDoc,
   doc,
 } from "firebase/firestore";
-import { useContext, useEffect, useState } from "react";
+import Comments from "../components/Comments";
 import { AuthContext } from "../context/AuthContext";
-import { db } from "../config/firebaseConfig";
 import parse from "html-react-parser";
-
+import { RecipeGeneral, userFavs } from "../types/types";
 import "../style/recipeDetails.scss";
 
 interface LocationState {
@@ -65,77 +64,81 @@ function RecipeDetails() {
   return (
     <>
       <main>
-        <section className="recipe-header-section">
-          <h3>{recipe.title}</h3>
-          {/* FAVS BUTTON DEPENDING ON STATE */}
-          {favs?.length != 0 ? (
-            <button onClick={handleAddFavourite}>
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/forkit-d574f.appspot.com/o/Full_Star.png?alt=media&token=cd7054c0-b436-4a17-a80d-95b0a8b0b951"
-                alt="empty star"
-                className="add-remove-fav-star"
-              />
-              Remove from favourites
-            </button>
-          ) : (
-            <button onClick={handleAddFavourite}>
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/forkit-d574f.appspot.com/o/Empty_Star.png?alt=media&token=297da907-8326-4f14-95a7-ecb42c39853c"
-                alt="empty star"
-                className="add-remove-fav-star"
-              />
-              Add to favourites
-            </button>
-          )}
-        </section>
+        <div className="recipe-details">
+          <section className="recipe-details__header-section">
+            <h3>{recipe.title}</h3>
+            {/* FAVS BUTTON DEPENDING ON STATE */}
+            {favs?.length != 0 ? (
+              <button onClick={handleAddFavourite}>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/forkit-d574f.appspot.com/o/Full_Star.png?alt=media&token=cd7054c0-b436-4a17-a80d-95b0a8b0b951"
+                  alt="empty star"
+                  className="add-remove-fav-star"
+                />
+                Remove from favourites
+              </button>
+            ) : (
+              <button onClick={handleAddFavourite}>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/forkit-d574f.appspot.com/o/Empty_Star.png?alt=media&token=297da907-8326-4f14-95a7-ecb42c39853c"
+                  alt="empty star"
+                  className="add-remove-fav-star"
+                />
+                Add to favourites
+              </button>
+            )}
+          </section>
 
-        <section className="recipe-info-section">
-          <p>
-            <strong>Health score: </strong>
-            {recipe.healthScore}
-          </p>
-          <p>
-            <strong>Ready in: </strong>
-            {recipe.readyInMinutes} min.
-          </p>
-          <p>
-            <strong>Servings: </strong>
-            {recipe.servings}
-          </p>
-          <p>
-            <strong>Sustainable: </strong>
-            {recipe.sustainable ? "Yes" : "No"}
-          </p>
-        </section>
+          <section className="recipe-details__info-section">
+            <p>
+              <strong>Health score: </strong>
+              {recipe.healthScore}
+            </p>
+            <p>
+              <strong>Ready in: </strong>
+              {recipe.readyInMinutes} min.
+            </p>
+            <p>
+              <strong>Servings: </strong>
+              {recipe.servings}
+            </p>
+            <p>
+              <strong>Sustainable: </strong>
+              {recipe.sustainable ? "Yes" : "No"}
+            </p>
+          </section>
 
-        <section>
-          <p>{parse(recipe.summary)}</p>
-        </section>
+          <section className="recipe-details__description-section">
+            <img src={recipe.image} />
+            <p>{parse(recipe.summary)}</p>
+          </section>
 
-        <div className="text-center">
-          <img src={recipe.image} width={"300px"} />
+          <section className="recipe-details__ingredients-section">
+            <h4>Ingredient list:</h4>
+            <ul>
+              {recipe.extendedIngredients.map((ingredient, indRec) => {
+                return (
+                  <li key={indRec}>
+                    <strong>{ingredient.nameClean}</strong> -{" "}
+                    {Math.floor(ingredient.measures.metric.amount)}{" "}
+                    {ingredient.measures.metric.unitShort}.
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+
+          <section className="recipe-details__instructions-section">
+            <h4>Instructions: </h4>
+            <ol>
+              {recipe.analyzedInstructions[0].steps.map((step, idx) => {
+                return <li key={idx}>{step.step}</li>;
+              })}
+            </ol>
+          </section>
+
+          <Comments recipeId={recipe.id} />
         </div>
-
-        <h4 className="text-center sectionsStyle">Ingredient list:</h4>
-        <ul>
-          {recipe.extendedIngredients.map((ingredient, indRec) => {
-            return (
-              <li key={indRec}>
-                {ingredient.original} ({ingredient.measures.metric.amount}{" "}
-                {ingredient.measures.metric.unitShort})
-              </li>
-            );
-          })}
-        </ul>
-
-        <h4 className="text-center sectionsStyle">Instructions: </h4>
-        <ol>
-          {recipe.analyzedInstructions[0].steps.map((step, idx) => {
-            return <li key={idx}>{step.step}</li>;
-          })}
-        </ol>
-
-        <Comments recipeId={recipe.id} />
       </main>
     </>
   );
