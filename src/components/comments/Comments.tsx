11a264ab0d24660645dfba5d4d5990add8cd8596
RZajacc <rf.zajac@gmail.com>
2024-04-23 18:10:43 +0200

    added styling to form form new comment, and useRef to clean the content of the text field. Also added a button style mixin and applied it in all places where I used same styling for the button

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -7,7 +7,7 @@ import {
   orderBy,
 } from "firebase/firestore";
 import { db } from "../../config/firebaseConfig";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Comment from "./comment/Comment";
 import { commentsType } from "../../types/types";
 import { AuthContext } from "../../context/AuthContext";
@@ -21,16 +21,19 @@ type Props = {
 function Comments({ recipeId }: Props) {
   const [comments, setComments] = useState<commentsType[] | null>(null);
   const { user } = useContext(AuthContext);
+  const commentInput = useRef<HTMLTextAreaElement>(null);
 
   // Add new comment
   const submitNewComment = async (e: React.FormEvent<HTMLFormElement>) => {
     // ! This part was necessary because data varied depending if user logged in with google or account created here
     // ! But in the end I will stick to just manually created accounts so it will be modified later
     e.preventDefault();
+
     // Get the form data
     const formData = new FormData(e.currentTarget);
     const commentData = formData.get("comment_text") as string;
 
+    // Collect user data
     const authorData = user
       ? user.displayName
         ? user.displayName
@@ -41,6 +44,8 @@ function Comments({ recipeId }: Props) {
         ? user.photoURL
         : "https://firebasestorage.googleapis.com/v0/b/forkit-d574f.appspot.com/o/noUser.png?alt=media&token=37607a3a-371b-40a9-9947-a0005991680b"
       : "No user";
+
+    // Create new comment data
     const newComment: commentsType = {
       authorID: user!.uid,
       recipeID: recipeId,
@@ -52,6 +57,8 @@ function Comments({ recipeId }: Props) {
 
     // Add a new document with a generated id.
     await addDoc(collection(db, "Comments"), newComment);
+    // Reset the input value
+    commentInput.current!.value = "";
   };
 
   useEffect(() => {
@@ -78,18 +85,21 @@ function Comments({ recipeId }: Props) {
   return (
     <>
       <h4 className="text-center">Comments:</h4>
+      {/* Display all comments */}
       <div className="comments-grid">
         {comments &&
           comments.map((comment, idx) => {
             return <Comment key={idx} comment={comment} />;
           })}
       </div>
-      {/* ! lets go here now */}
-      <form onSubmit={submitNewComment}>
+      {/* Add new comment */}
+      <form onSubmit={submitNewComment} className="comment-form">
+        <h5 className="text-center">Add new comment:</h5>
         <textarea
           placeholder="Leave your comment here"
           rows={3}
           name="comment_text"
+          ref={commentInput}
         />
         <button type="submit">Add comment</button>
       </form>
