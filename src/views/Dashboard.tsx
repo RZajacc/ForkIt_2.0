b8aedb887f46af1def1e22a12906f1a67015aa69
RZajacc <rf.zajac@gmail.com>
2024-04-25 17:16:07 +0200

    added main elements to dashboard and register view since all my views depend on it and deleted last css file

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,5 +1,4 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
-import "../style/Dashboard.css";
 import DashboardUser from "../components/DashboardUser";
 import DashboardFavs from "../components/DashboardFavs";
 import { useState } from "react";
@@ -9,33 +8,35 @@ function Dashboard() {
 
   return (
     <>
-      <Container className="dashboard-container">
-        <Row className="justify-content-md-center dashboard-content">
-          <Col className="dashboard-top-content">
-            <Button
-              variant="info"
-              className="dashboard-button"
-              onClick={() => {
-                setDisplayToggle(true);
-              }}
-            >
-              Account details
-            </Button>
-          </Col>
-          <Col className="dashboard-top-content">
-            <Button
-              variant="info"
-              className="dashboard-button"
-              onClick={() => {
-                setDisplayToggle(false);
-              }}
-            >
-              Saved recipes
-            </Button>
-          </Col>
-        </Row>
-        {displayToggle ? <DashboardUser /> : <DashboardFavs />}
-      </Container>
+      <main>
+        <Container className="dashboard-container">
+          <Row className="justify-content-md-center dashboard-content">
+            <Col className="dashboard-top-content">
+              <Button
+                variant="info"
+                className="dashboard-button"
+                onClick={() => {
+                  setDisplayToggle(true);
+                }}
+              >
+                Account details
+              </Button>
+            </Col>
+            <Col className="dashboard-top-content">
+              <Button
+                variant="info"
+                className="dashboard-button"
+                onClick={() => {
+                  setDisplayToggle(false);
+                }}
+              >
+                Saved recipes
+              </Button>
+            </Col>
+          </Row>
+          {displayToggle ? <DashboardUser /> : <DashboardFavs />}
+        </Container>
+      </main>
     </>
   );
 }
--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -61,54 +61,58 @@ function Register() {
     }
   };
   return (
-    <Col xxl="4" xl="5" lg="5" md="10" sm="10" xs="10" className="colStyle">
-      <Form onSubmit={handleRegister} className="text-center">
-        <h5>If you don't have an account yet please use this register form:</h5>
-        <Form.Group className="mb-3" controlId="register-email">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            onChange={handleEmailChange}
-            required
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
+    <main>
+      <Col xxl="4" xl="5" lg="5" md="10" sm="10" xs="10" className="colStyle">
+        <Form onSubmit={handleRegister} className="text-center">
+          <h5>
+            If you don't have an account yet please use this register form:
+          </h5>
+          <Form.Group className="mb-3" controlId="register-email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={handleEmailChange}
+              required
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            onChange={handlePasswordChange}
-          />
-          <Form.Label>Repeat password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="confirm password"
-            onChange={handleConfirmPasswrodChange}
-          />
-        </Form.Group>
-        <div className="reg-errors">
-          {passwordErr &&
-            passwordErr.map((err, idx) => {
-              return <p key={idx}>{err}</p>;
-            })}
-        </div>
-        <Button variant="primary" type="submit" className="loginButtonStyle">
-          Register
-        </Button>
-        <div className="password-req">
-          <p>*Password needs to have at least 8 characters</p>
-          <p>*Password needs to contain at least 1 uppercase character</p>
-          <p>*Password needs to contain at least 1 number</p>
-          <p>*Password needs to contain at least 1 special character</p>
-        </div>
-      </Form>
-    </Col>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={handlePasswordChange}
+            />
+            <Form.Label>Repeat password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="confirm password"
+              onChange={handleConfirmPasswrodChange}
+            />
+          </Form.Group>
+          <div className="reg-errors">
+            {passwordErr &&
+              passwordErr.map((err, idx) => {
+                return <p key={idx}>{err}</p>;
+              })}
+          </div>
+          <Button variant="primary" type="submit" className="loginButtonStyle">
+            Register
+          </Button>
+          <div className="password-req">
+            <p>*Password needs to have at least 8 characters</p>
+            <p>*Password needs to contain at least 1 uppercase character</p>
+            <p>*Password needs to contain at least 1 number</p>
+            <p>*Password needs to contain at least 1 special character</p>
+          </div>
+        </Form>
+      </Col>
+    </main>
   );
 }
 
