406ef022d9973eaa8fca3ba343f8f89b7ccaf9c1
RZajacc <rf.zajac@gmail.com>
2024-04-30 14:08:13 +0200

    added a file size validation

--- src/components/dashboard/user/DashboardUser.tsx
+++ src/components/dashboard/user/DashboardUser.tsx
@@ -1,7 +1,6 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 import { v4 } from "uuid";
-
 import "./dashboard-user.scss";
 import {
   deleteObject,
@@ -15,6 +14,10 @@ import { updateProfile } from "firebase/auth";
 
 function DashboardUser() {
   const { user, setUser } = useContext(AuthContext);
+  const [fileSizeError, setFileSizeError] = useState(0);
+  const [fileSizeErrorClass, setFileSizeErrorClass] = useState(
+    "upload-progress--hidden"
+  );
   const [uploadProgress, setUploadProgress] = useState(0);
   const [uploadProgressClass, setUploadProgressClass] = useState(
     "upload-progress--hidden"
@@ -35,7 +38,11 @@ function DashboardUser() {
     // Designing image resulting name
     const imgNameSplit = userImg.name.split(".");
     const imgName = imgNameSplit[0] + "_" + v4() + "." + imgNameSplit[1];
+
+    // Resetting classes
     setUploadErrorClass("upload-progress--hidden");
+    setFileSizeErrorClass("upload-progress--hidden");
+
     // Create a root reference
     const storage = getStorage();
 
@@ -44,51 +51,57 @@ function DashboardUser() {
 
     const uploadTask = uploadBytesResumable(storageRef, userImg);
 
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setUploadProgress(progress);
-        setUploadProgressClass("upload-progress--active");
-      },
-      (error: FirebaseError) => {
-        setUploadErrorClass("upload-error--active");
-        setUploadError(error.message);
-      },
-      () => {
-        setUploadProgressClass("upload-progress--hidden");
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL: string) => {
-          const userImageURL = user?.photoURL;
-          // If its default one only update the link but if custom, delete previous one
-          if (userImageURL === "/noUser.png") {
-            updateProfile(user!, { photoURL: downloadURL }).then(() => {
-              setUser(user);
-              setUserImg(downloadURL);
-            });
-          } else {
-            updateProfile(user!, { photoURL: downloadURL }).then(() => {
-              setUser(user);
-              setUserImg(downloadURL);
-            });
-            const img = user?.photoURL as string;
-            const deleteRef = ref(storage, img);
-            deleteObject(deleteRef)
-              .then(() => {
-                console.log("Success");
-              })
-              .catch((error: FirebaseError) => {
-                console.log("ERROR", error.message);
-              });
-          }
-        });
-      }
-    );
+    if (userImg.size > 3000000) {
+      setFileSizeErrorClass("file-size-error--active");
+      setFileSizeError(userImg.size / 1000000);
+    } else {
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setUploadProgress(progress);
+          setUploadProgressClass("upload-progress--active");
+        },
+        (error: FirebaseError) => {
+          setUploadErrorClass("upload-error--active");
+          setUploadError(error.message);
+        },
+        () => {
+          setUploadProgressClass("upload-progress--hidden");
+          getDownloadURL(uploadTask.snapshot.ref).then(
+            (downloadURL: string) => {
+              const userImageURL = user?.photoURL;
+              // If its default one only update the link but if custom, delete previous one
+              if (userImageURL === "/noUser.png") {
+                updateProfile(user!, { photoURL: downloadURL }).then(() => {
+                  setUser(user);
+                  setUserImg(downloadURL);
+                });
+              } else {
+                updateProfile(user!, { photoURL: downloadURL }).then(() => {
+                  setUser(user);
+                  setUserImg(downloadURL);
+                });
+                const img = user?.photoURL as string;
+                const deleteRef = ref(storage, img);
+                deleteObject(deleteRef)
+                  .then(() => {
+                    console.log("Success");
+                  })
+                  .catch((error: FirebaseError) => {
+                    console.log("ERROR", error.message);
+                  });
+              }
+            }
+          );
+        }
+      );
+    }
   };
   return (
     <>
       <div className="user-image-container">
-        {/* <img src={user?.photoURL ? user.photoURL : ""} /> */}
         <img src={userImg ? userImg : ""} />
       </div>
       <form
@@ -97,6 +110,7 @@ function DashboardUser() {
         className="user-image-form"
       >
         <input type="file" name="user-image" accept="image/png, image/jpeg" />
+        <small>*File is not supposed to be bigger than 3Mb.</small>
         <button type="submit">Upload</button>
       </form>
       <div>
@@ -104,6 +118,9 @@ function DashboardUser() {
           Upload progress: <span>{Math.floor(uploadProgress) + "%"}</span>
         </p>
         <p className={uploadErrorClass}>{uploadError}</p>
+        <p className={fileSizeErrorClass}>
+          Your file is too big! <span>It's {fileSizeError.toFixed(1)} Mb!</span>
+        </p>
       </div>
       <div>
         <p>
