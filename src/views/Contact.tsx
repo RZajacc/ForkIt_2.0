34be7e4784fbd649cdef38934a156829f98a080d
RZajacc <rf.zajac@gmail.com>
2024-05-02 16:15:09 +0200

    finished styling contact form

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,10 +1,15 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 import "../style/contact.scss";
 
 function Contact() {
   const form = useRef<HTMLFormElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+  const [result, setResult] = useState("");
+  const [resultClass, setResultClass] = useState("hidden");
 
   // Emailjs credentials
   const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
@@ -13,17 +18,23 @@ function Contact() {
 
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+    setResult("");
+    setResultClass("hidden");
     emailjs
       .sendForm(SERVICE_ID, TEMPLATE_ID, form.current!, {
         publicKey: PUBLIC_KEY,
       })
       .then(
         () => {
-          console.log("SUCCESS!");
+          setResult("Message sent successfully!");
+          setResultClass("success");
+          nameRef.current!.value = "";
+          emailRef.current!.value = "";
+          messageRef.current!.value = "";
         },
         (error) => {
-          console.log("FAILED...", error.text);
+          setResult("FAILED... " + error.text);
+          setResultClass("failure");
         }
       );
   };
@@ -34,15 +45,15 @@ function Contact() {
           <h4>If you have any questions send me a message:</h4>
           <form ref={form} onSubmit={sendEmail}>
             <label>Name</label>
-            <input type="text" name="user_name" />
+            <input type="text" name="user_name" ref={nameRef} required />
             <label>Email</label>
-            <input type="email" name="user_email" />
+            <input type="email" name="user_email" ref={emailRef} required />
             <label>Message</label>
-            <textarea name="message" rows={5} />
+            <textarea name="message" rows={5} ref={messageRef} required />
             <button type="submit">Send</button>
+            <p className={resultClass}>{result}</p>
           </form>
         </section>
-        <section className="feedback-section"></section>
       </main>
     </>
   );
